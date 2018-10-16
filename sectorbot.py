fe092752dcbfac8bcf5071741d1a23c287ff3593
Relyk-x <43947707+Relyk-x@users.noreply.github.com>
2018-10-16 11:41:58 +0800

    Update sectorbot.py

--- sectorbot.py
+++ sectorbot.py
@@ -14,8 +14,8 @@ client = commands.Bot(command_prefix = ';')
 Clientdiscord = discord.Client()
 servers = list(client.servers)
 
-status = ['for: s/help | v0.2.2', 'for bot suggestions', 'for @Relyk-x#2896']
-# WATCHING 'over ' + str(len(bot.servers)) + ' servers' ## v0.2.2,5##
+status = ['for: ;help | v0.2.2', 'for bot suggestions', 'for @Relyk-x#2896']
+# WATCHING 'over ' + str(len(bot.servers)) + ' servers' ## v0.2.2,7##
 
 async def change_status():
   await client.wait_until_ready()
