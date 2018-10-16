bd0e4a4a12b275186cdb4efb4297ae71711ce3d3
Relyk-x <43947707+Relyk-x@users.noreply.github.com>
2018-10-17 00:26:12 +0800

    Update sectorbot.py

--- sectorbot.py
+++ sectorbot.py
@@ -1,6 +1,5 @@
 # Initialise
 import discord
-from discord.ext.commands import Bot
 from discord.ext import commands
 import asyncio
 import time
@@ -13,10 +12,11 @@ Client = discord.client
 client = commands.Bot(command_prefix = ';')
 Clientdiscord = discord.Client()
 servers = list(client.servers)
-
+content = message.content
 status = ['for: ;help | v0.2.3', 'for: bot suggestions', 'for: @Relyk-x#2896']
-# WATCHING 'over ' + str(len(bot.servers)) + ' servers' ## v0.2.3,0##
+# WATCHING 'over ' + str(len(bot.servers)) + ' servers' ## v0.2.3,7##
 
+# Setting Bot status 'Watching'
 async def change_status():
   await client.wait_until_ready()
   msgs = cycle(status)
@@ -33,27 +33,14 @@ async def on_member_join(member):
     for x in range(len(servers)):
      print(' ' + servers[x-1].name)
     await client.send_message(member, "Hey there I'm Sector Bot")
+    
+# Checking if bot is online    
 async def on_ready():
    print('Sector Bot is up and running with ' + str(len(client.servers)) + ' servers connected!')
-    
-# Purge
-#@client.event
-#async def on_purge(message):
-#    if message.content.startswith('s/purge ') and not message.content[8:]=='':
-#        message_amount = int(message.content[8:])
-#        deleted = await bot.purge_from(message.channel, limit=message_amount, check=on_purge)
-#       await client.send_message(message.channel, 'Deleted {} message(s)'.format(len(deleted)))
-# Say
-#@client.event
-#async def on_say(message):
-#    if content.startswith('s/say '):
-#        con = message.content
-#        await client.send_message(message.channel, con[6:])
 
 # Multiple Commands
 @client.event
 async def on_message(message):
-    content = message.content
     
     # Purge
     if message.content.startswith(';purge ') and not message.content[7:]=='':
@@ -64,10 +51,12 @@ async def on_message(message):
         selfdel = await client.send_message(message.channel, embed=em)
         await asyncio.sleep(10)
         await client.delete_message(selfdel)
+        
     # Say
     if content.startswith(';say '):
         await client.send_message(message.channel, content[5:])
         await client.delete_message(message)
+        
     # Memes
     if (';meme') in message.content:
         randomlist = ['https://goo.gl/dwJD8o',  #Batman
@@ -90,206 +79,7 @@ async def on_message(message):
         em.set_image(url='%s' %(random.choice(randomlist),))
         em.set_author(name="Sector Bot", icon_url="https://goo.gl/34WWBc")
         await client.send_message(message.channel, embed=em)
-    # Emoji Lettering
-    if message.content.startswith(';emojify '):
-        result = ''
-        for letter in message.content[9:]:
-            if letter in 'aA':
-                result = result + '🅰'
-            if letter in 'bB':
-                result = result + '🅱'
-            if letter in 'cC':
-                result = result + '🅲'
-            if letter in 'dD':
-                result = result + '🅳'
-            if letter in 'eE':
-                result = result + '🅴'
-            if letter in 'fF':
-                result = result + '🅵'
-            if letter in 'gG':
-                result = result + '🅶'
-            if letter in 'hH':
-                result = result + '🅷'
-            if letter in 'iI':
-                result = result + '🅸'
-            if letter in 'jJ':
-                result = result + '🅹'
-            if letter in 'Kk':
-                result = result + '🅺'
-            if letter in 'lL':
-                result = result + '🅻'
-            if letter in 'mM':
-                result = result + '🅼'
-            if letter in 'nN':
-                result = result + '🅽'
-            if letter in 'oO':
-                result = result + '🅾'
-            if letter in 'pP':
-                result = result + '🅿'
-            if letter in 'qQ':
-                result = result + '🆀'
-            if letter in 'rR':
-                result = result + '🆁'
-            if letter in 'sS':
-                result = result + '🆂'
-            if letter in 'tT':
-                result = result + '🆃'
-            if letter in 'uU':
-                result = result + '🆄'
-            if letter in 'Vv':
-                result = result + '🆅'
-            if letter in 'wW':
-                result = result + '🆆'
-            if letter in 'xX':
-                result = result + '🆇'
-            if letter in 'Yy':
-                result = result + '🆈'
-            if letter in 'zZ':
-                result = result + '🆉'
-            if letter in '1':
-                result = result + '1️⃣'
-            if letter in '2':
-                result = result + '2️⃣'
-            if letter in '3':
-                result = result + '3️⃣'
-            if letter in '4':
-                result = result + '4️⃣'
-            if letter in '5':
-                result = result + '5️⃣'
-            if letter in '6':
-                result = result + '6️⃣'
-            if letter in '7':
-                result = result + '7️⃣'
-            if letter in '8':
-                result = result + '8️⃣'
-            if letter in '9':
-                result = result + '9️⃣'
-            if letter in '0':
-                result = result + '0️⃣'
-            if letter in ' ':
-                result = result + ' '
-            if letter in '@':
-                result = result + '🕒'
-            if letter in '!':
-                result = result + '❗️'
-            if letter in '.':
-                result = result + '▫️'
-            if letter in ',':
-                result = result + '🔸'
-            if letter in '#':
-                result = result + '#️⃣'
-            if letter in '$':
-                result = result + '💲'
-            if letter in '%':
-                result = result + '💮'
-            if letter in '^':
-                result = result + '🔺'
-            if letter in '&':
-                result = result + '🌀'
-            if letter in '?':
-                result = result + '❔'
-        await client.send_message(message.channel, result)
-        await client.delete_message(message)
-    # Tiny Letters
-    if message.content.startswith(';compress '):
-        result = ''
-        for letter in message.content[10:]:
-            if letter in 'aA':
-                result = result + 'ᴀ'
-            if letter in 'bB':
-                result = result + 'ʙ'
-            if letter in 'cC':
-                result = result + 'ᴄ'
-            if letter in 'dD':
-                result = result + 'ᴅ'
-            if letter in 'eE':
-                result = result + 'ᴇ'
-            if letter in 'fF':
-                result = result + 'ғ'
-            if letter in 'gG':
-                result = result + 'ɢ'
-            if letter in 'hH':
-                result = result + 'ʜ'
-            if letter in 'iI':
-                result = result + 'ɪ'
-            if letter in 'jJ':
-                result = result + 'ᴊ'
-            if letter in 'Kk':
-                result = result + 'ᴋ'
-            if letter in 'lL':
-                result = result + 'ʟ'
-            if letter in 'mM':
-                result = result + 'ᴍ'
-            if letter in 'nN':
-                result = result + 'ɴ'
-            if letter in 'oO':
-                result = result + 'ᴏ'
-            if letter in 'pP':
-                result = result + 'ᴘ'
-            if letter in 'qQ':
-                result = result + 'ǫ'
-            if letter in 'rR':
-                result = result + 'ʀ'
-            if letter in 'sS':
-                result = result + 's'
-            if letter in 'tT':
-                result = result + 'ᴛ'
-            if letter in 'uU':
-                result = result + 'ᴜ'
-            if letter in 'Vv':
-                result = result + 'ᴠ'
-            if letter in 'wW':
-                result = result + 'ᴡ'
-            if letter in 'xX':
-                result = result + 'x'
-            if letter in 'Yy':
-                result = result + 'ʏ'
-            if letter in 'zZ':
-                result = result + 'ᴢ'
-            if letter in '1':
-                result = result + '➊'
-            if letter in '2':
-                result = result + '➋'
-            if letter in '3':
-                result = result + '➌'
-            if letter in '4':
-                result = result + '➍'
-            if letter in '5':
-                result = result + '➎'
-            if letter in '6':
-                result = result + '➏'
-            if letter in '7':
-                result = result + '➐'
-            if letter in '8':
-                result = result + '➑'
-            if letter in '9':
-                result = result + '➒'
-            if letter in '0':
-                result = result + '⓪'
-            if letter in ' ':
-                result = result + ' '
-            if letter in '@':
-                result = result + '@'
-            if letter in '!':
-                result = result + '!'
-            if letter in '.':
-                result = result + '.'
-            if letter in ',':
-                result = result + ','
-            if letter in '#':
-                result = result + '#'
-            if letter in '$':
-                result = result + '$'
-            if letter in '%':
-                result = result + '%'
-            if letter in '^':
-                result = result + '^'
-            if letter in '&':
-                result = result + '&'
-            if letter in '?':
-                result = result + '?'
-        await client.send_message(message.channel, result)
-        await client.delete_message(message)
+        
     # Password Generator
     if (';password') in message.content:
         encryptkey = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',]
@@ -342,31 +132,37 @@ async def on_message(message):
         f = open('authpass.txt','a')
         f.write('\n' + 'Password: ' + encryptedpass + ' was generated by ' + str(message.author))
         f.close()
+        
     # Invite
     if (';invite') in message.content:
         em = discord.Embed(description="If you'd like to add Sector Bot to your server, Click here: https://goo.gl/2rp6n2", color=0xb8ff00)
         em.set_author(name="Sector Bot", icon_url="https://goo.gl/34WWBc")
         await client.send_message(message.channel, embed=em)
+        
     # Server Count
     if (';servercount') in message.content:
-        em = discord.Embed(description='Currently watching over ' + str(len(client.servers)) + ' servers <:discordp:500719998172659713>', color=0xb8ff00)
+        em = discord.Embed(description='Currently watching over ' + str(len(client.servers)) + 'Discord servers <:discordp:500719998172659713>', color=0xb8ff00)
         em.set_author(name="Sector Bot", icon_url="https://goo.gl/34WWBc")
         await client.send_message(message.channel, embed=em)
+        
     # Hello
     if (';greet') in message.content:
         em = discord.Embed(description='Hey there buddy! :wave:', color=0xb8ff00)
         em.set_author(name="Sector Bot", icon_url="https://goo.gl/34WWBc")
         await client.send_message(message.channel, embed=em)
+        
     # Vote
     if (';vote') in message.content:
         em = discord.Embed(description='You can vote here: https://discordbots.org/bot/496214977267630080/vote', color=0xb8ff00)
         em.set_author(name="Sector Bot", icon_url="https://goo.gl/34WWBc")
         await client.send_message(message.channel, embed=em)
+        
 ##### Version
     if (';version') in message.content:
         em = discord.Embed(description='The current version of Sector Bot is: `v0.2.3`', color=0xb8ff00)
         em.set_author(name="Sector Bot", icon_url="https://goo.gl/34WWBc")
         await client.send_message(message.channel, embed=em)
+    
     # Dice Roll
     if (';diceroll') in message.content or (';dr') in message.content:
         randomlist = ['1','2','3','4','5','6',]
@@ -410,7 +206,8 @@ async def on_message(message):
         await client.send_message(message.channel, embed=em)
     # Russian Roulette
         # coming soon #
-# Helo
+      
+# Help
     if (';help') in message.content:
         em = discord.Embed(title="Discord Server", description="For any other help please join our Discord server...", url="https://discord.gg/eRHsyFg", color=0xb8ff00)
         em.set_author(name="Sector Bot", icon_url="https://goo.gl/34WWBc")
@@ -422,8 +219,6 @@ async def on_message(message):
         em.add_field(name="version", value="The current version of Sector Bot", inline=False)
         em.add_field(name="vote", value="Vote for this bot.", inline=False)
         em.add_field(name="meme", value="Sends a random meme from Sector Bot’s stash.", inline=False)
-        em.add_field(name="emojify <text>", value="Emojifies your text.", inline=False)
-        em.add_field(name="compress <text>", value="compresses your text to make it smaller.", inline=False)
         em.add_field(name="say <text>", value="rewrites your text.", inline=False)
         em.add_field(name="purge <amount>", value="purges a specific amount of messages in a channel", inline=False)
         em.add_field(name="password", value="Generates a random password.", inline=False)
