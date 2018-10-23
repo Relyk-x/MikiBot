b7d8f704a848db5933dcfb7ed4e178be959c9471
Relyk-x <43947707+Relyk-x@users.noreply.github.com>
2018-10-23 22:36:22 +0800

    Update sectorbot.py

--- sectorbot.py
+++ sectorbot.py
@@ -75,6 +75,7 @@ async def on_message(message):
                       'https://goo.gl/UFNkSe',  #Shaggy, Weed
                     ]
         em = discord.Embed(title= 'Meme:', color=0xe9b820,)
+        em.set_image(url='%s' %(random.choice(randomlist),))
         await client.send_message(message.channel, embed=em)
         
     # Password Generator
