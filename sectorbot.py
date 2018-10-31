30d8d285bea90ff69579192a11dd1a09f439cab1
Relyk-x <43947707+Relyk-x@users.noreply.github.com>
2018-10-31 15:42:39 +0800

    Update sectorbot.py

--- sectorbot.py
+++ sectorbot.py
@@ -64,8 +64,6 @@ async def on_message(message):
         servers = list(client.servers)
         em = discord.Embed(title="- click here for help -", url="https://discord.gg/eRHsyFg", description="For any other help please join our Discord server...", color=0xffafc9)
         em.set_author(name="MikiBot", url="https://cdn.discordapp.com/attachments/499771950764261396/506802847791185920/miki2.png", icon_url="https://cdn.discordapp.com/attachments/499771950764261396/506802847791185920/miki2.png")
-        em.add_field(name="Server Count", value="Watching over " + str(len(client.servers)) + " servers:")
-        await client.send_message(message.channel, embed=em)
         for x in range(len(servers)):
          em.add_field(name="Server List", value='● ' + servers[x-1].name) + ' [ServerID]: ' + servers[x-1].id)
          await client.send_message(message.channel, embed=em)
