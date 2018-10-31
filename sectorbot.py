6aeee57e080a4c3c2f79d9fb779f2d8883ffb852
Relyk-x <43947707+Relyk-x@users.noreply.github.com>
2018-10-31 13:17:30 +0800

    Update sectorbot.py

--- sectorbot.py
+++ sectorbot.py
@@ -466,7 +466,7 @@ async def on_message(message):
         em = discord.Embed(title="- click here for help -", description="For any other help please join our Discord server...", url="https://discord.gg/eRHsyFg", color=0xffafc9)
         em.set_author(name="MikiBot", icon_url="https://cdn.discordapp.com/attachments/499771950764261396/506802847791185920/miki2.png")
         # Bot
-        em.add_field(name="Bot 🤖", value=" Prefix: `;` \n Commands: `;<command>` \n ════════════════════════════════════════", inline=True)
+        em.add_field(name="Bot", value=" Prefix: `;` \n Commands: `;<command>` \n ════════════════════════════════════════", inline=True)
         
         # Social
         em.add_field(name="Social 💬", value=" hello – Sends a greeting in the channel. \n oofify – Emojifies your text. \n tiny – Decorates your text. \n say <text> – rewrites your text. \n ════════════════════════════════════════", inline=False)
@@ -479,7 +479,7 @@ async def on_message(message):
         em.add_field(name="Fun 🎉", value=" meme – Sends a random meme from Sector Bot’s stash. \n password – Generates a random password. \n randimg – Generates a random image. \n randwallpaper – Generate a random wallpaper. \n blurimg – Generate a random blury image. \n blurwallpaper – Generate a random blury wallpaper. \n gsimg – Generate a random greyscale image. \n gswallpaper – Generate a random greyscale wallpaper. \n randgif – Generate a random gif. \n ════════════════════════════════════════", inline=False)
         
         #Games
-        em.add_field(name="Games 🎮", value=" diceroll – Rolls a six sided die. \n coinflip – Flips a coin, could be heads could be tails. \n 8ball – Ask a question and shake the 8 Ball. \n ════════════════════════════════════════", inline=False)
+        em.add_field(name="Games 🎭", value=" diceroll – Rolls a six sided die. \n coinflip – Flips a coin, could be heads could be tails. \n 8ball – Ask a question and shake the 8 Ball. \n ════════════════════════════════════════", inline=False)
         
         em.set_footer(text="version: v0.3.0")
         await client.send_message(message.channel, embed=em)
