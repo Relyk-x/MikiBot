bef43d2f741a3e225b7af2d130d5fcedc6704f4a
Relyk_x <43947707+Relyk-x@users.noreply.github.com>
2018-11-11 20:39:35 +0800

    Update mikibot.py

--- mikibot.py
+++ mikibot.py
@@ -443,7 +443,7 @@ async def on_message(message):
         a0 = [ x for x in div[0].find_all('a') if x.has_attr('title') ][0]
         url= ('http://www.youtube.com'+a0['href'])
         em = discord.Embed(title=title, url=url, color=0xffafc9)
-        em.set_author(name='YouTube', icon_url='https://cdn.discordapp.com/attachments/499771919059648588/511155784055521285/yt.png')
+        em.set_author(name='<:yt:511157819689533440> YouTube')
         em.set_thumbnail(url=imgurl)
         em.add_field(name='YouTube Channel', value='<channel name>', inline=True)
         em.add_field(name='Duration', value='<duration of video>', inline=True)
