0031060752e7f89ab6a1f92da8db252d67a83ba6
Relyk_x <43947707+Relyk-x@users.noreply.github.com>
2018-11-11 20:52:29 +0800

    Update mikibot.py

--- mikibot.py
+++ mikibot.py
@@ -443,11 +443,11 @@ async def on_message(message):
         a0 = [ x for x in div[0].find_all('a') if x.has_attr('title') ][0]
         url= ('http://www.youtube.com'+a0['href'])
         em = discord.Embed(title=title, url=url, color=0xffafc9)
-        em.set_author(name='<:yt:511157819689533440> YouTube')
+        em.set_author(name=':mag: YouTube Search')
         em.set_thumbnail(url=imgurl)
         em.add_field(name='YouTube Channel', value='<channel name>', inline=True)
         em.add_field(name='Duration', value='<duration of video>', inline=True)
-        em.set_footer(text="~ Powered by Converse Foundation")
+        em.set_footer(text="not yet opperational...")
         await client.send_message(message.channel, embed=em)
     
     # Dice Roll
