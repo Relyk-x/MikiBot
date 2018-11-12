27b1ec0af97e042d79eb12784f4fb1b0636e117b
Relyk_x <43947707+Relyk-x@users.noreply.github.com>
2018-11-12 12:19:20 +0800

    Update mikibot.py

--- mikibot.py
+++ mikibot.py
@@ -13,7 +13,7 @@ from time import gmtime, strftime
 
 FORTNITE_API_TOKEN = os.getenv('FORTNITETOKEN')
 COMMAND_PREFIX = ';'
-VERSION = 'v0.4.2' #v0.4.2,9
+VERSION = 'v0.4.2' #v0.4.3,0
 
 querystring = {"format":"json"}
 
@@ -533,7 +533,7 @@ async def on_message(message):
           kd = res[4]['value']
 
           em = discord.Embed(description="In game Stats for " + words[2] + ':', color=0xffafc9)
-          em.set_author(title='⚔️  Fortnite')
+          em.set_author(name='⚔️  Fortnite')
           em.set_thumbnail(url='https://cdn.discordapp.com/attachments/499771919059648588/511373423541354497/fortnite.png')
           em.add_field(name="Matches Played", value=matches_played + '\n', inline=False)
           em.add_field(name="Wins", value=wins + '\n', inline=True)
