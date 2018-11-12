fb81665c4f6a3f97c4c852dc1563f8ca8412faae
Relyk_x <43947707+Relyk-x@users.noreply.github.com>
2018-11-12 12:13:55 +0800

    Update mikibot.py

--- mikibot.py
+++ mikibot.py
@@ -13,7 +13,7 @@ from time import gmtime, strftime
 
 FORTNITE_API_TOKEN = os.getenv('FORTNITETOKEN')
 COMMAND_PREFIX = ';'
-VERSION = 'v0.4.2' #v0.4.2,7
+VERSION = 'v0.4.2' #v0.4.2,8
 
 querystring = {"format":"json"}
 
@@ -496,8 +496,12 @@ async def on_message(message):
       words = message.content.split(' ', 2)
 
       if len(words) < 3:
-        em = discord.Embed(description='platform: pc | xbox | ps4 \nnickname: your fortnite player name')
-        em.add_field(name='Usage', value=COMMAND_PREFIX + 'fortnite | <platform> | <nickname> |')
+        em = discord.Embed(description='Error', color=0xffafc9)
+        em.set_author(title='⚔️  Fortnite')
+        em.set_thumbnail(url='https://cdn.discordapp.com/attachments/499771919059648588/511373423541354497/fortnite.png')
+        em.add_field(name='platform', value='pc | xbox | ps4', inline=True)
+        em.add_field(name='nickname', value='your fortnite player name', inline=True)
+        em.add_field(name='Usage', value=COMMAND_PREFIX + 'fortnite | <platform> | <nickname> |', inline=False)
         await client.send_message(message.channel, embed=em)
         return
 
@@ -510,8 +514,12 @@ async def on_message(message):
         platform = 'psn'
 
       if platform not in ('pc','xbl','psn'):
-        em = discord.Embed(description='platform: pc | xbox | ps4 \nnickname: your fortnite player name')
-        em.add_field(name='Usage', value=COMMAND_PREFIX + 'fortnite | <platform> | <nickname> |')
+        em = discord.Embed(description='Error', color=0xffafc9)
+        em.set_author(title='⚔️  Fortnite')
+        em.set_thumbnail(url='https://cdn.discordapp.com/attachments/499771919059648588/511373423541354497/fortnite.png')
+        em.add_field(name='platform', value='pc | xbox | ps4', inline=True)
+        em.add_field(name='nickname', value='your fortnite player name', inline=True)
+        em.add_field(name='Usage', value=COMMAND_PREFIX + 'fortnite | <platform> | <nickname> |', inline=False)
         await client.send_message(message.channel, embed=em)
         return
       else:
@@ -525,7 +533,7 @@ async def on_message(message):
           kd = res[4]['value']
 
           em = discord.Embed(description="In game Stats for " + words[2] + ':', color=0xffafc9)
-          em.set_author(name='⚔️  Fortnite')
+          em.set_author(title='⚔️  Fortnite')
           em.set_thumbnail(url='https://cdn.discordapp.com/attachments/499771919059648588/511373423541354497/fortnite.png')
           em.add_field(name="Matches Played", value=matches_played + '\n', inline=False)
           em.add_field(name="Wins", value=wins + '\n', inline=True)
