2fd6ce7ce73599e1aeb83d1272b79db6a9dc97e2
Relyk_x <43947707+Relyk-x@users.noreply.github.com>
2018-11-14 20:51:17 +0800

    Update mikibot.py

--- mikibot.py
+++ mikibot.py
@@ -13,7 +13,7 @@ from time import gmtime, strftime
 
 FORTNITE_API_TOKEN = os.getenv('FORTNITETOKEN')
 COMMAND_PREFIX = ';'
-VERSION = 'v0.4.7' #v0.4.7,3
+VERSION = 'v0.4.7' #v0.4.7,4
 
 Client = discord.client
 client = commands.Bot(command_prefix = ';')
@@ -341,15 +341,15 @@ async def on_message(message):
         await client.send_message(message.channel, embed=em)
     
     # Time
-    if message.content == ';time':
+    if message.content == 'b!time':
         dash = strftime("%H:%M", gmtime())
         wholetime = dash[0] + dash[1]
         resttime = dash[2:]
         if int(wholetime) < 12:
-          await client.send_message(message.channel, 'The server time now is: **' + wholetime + resttime + 'AM. GMTIME(0:00)**')
+            await client.send_message(message.channel, 'The server time now is: **' + wholetime + resttime + 'AM. GMTIME(0:00)**')
         else:
-          await client.send_message(message.channel, 'The server time now is: **' + int(wholetime-12) + resttime + 'PM. GMTTIME(0:00)**')
-    
+            await client.send_message(message.channel, 'The server time now is: **' + int(wholetime-12) + resttime + 'PM. GMTTIME(0:00)**')
+
     # Purge
     if message.content.startswith(';purge ') and not message.content[7:]=='':
         if message.author.id == '257784039795064833':
