64894232738ec36196bd7893cb855af1571e8f92
Relyk_x <43947707+Relyk-x@users.noreply.github.com>
2018-11-22 14:32:25 +0800

    Update mikibot.py

--- mikibot.py
+++ mikibot.py
@@ -16,19 +16,18 @@ import os
 bot = commands.Bot(command_prefix='m!')
 msglimit = 100
 now = datetime.now()
-version = "v0.0.0"
+version = "v0.4.8"#3
 
 @bot.event
 async def on_ready():
 	servers = list(bot.servers)
 	status = f"over {str(len(bot.servers))} servers"
-	print (f"MikiBot is up and running with {str(len(bot.servers))} servers connected!")
+	print (f"MikiBot is up on {str(len(bot.servers))} servers!")
+	print (f"Bot Name: {bot.user.name}")
+	print (f"Bot ID: {bot.user.id}")
 	print ("Ready when you are...")
-	print (f"I am running on {bot.user.name}")
-	print (f"With the ID: {bot.user.id}")
 	await bot.change_presence(game=discord.Game(name=status,type=3))
-# WATCHING 'over ' + str(len(bot.servers)) + ' servers', url="https://www.twitch.tv/streamer"
-
+	
 @bot.event
 async def on_member_join(member):
 	print('Sent message to ' + member.name)
@@ -144,6 +143,7 @@ async def count(ctx):
 @bot.command(pass_context=True)
 async def version(ctx):
 	embed = discord.Embed(description=f"The current version of Sector Bot is: `{version}`", color=0xffafc9)
+	embed.set_author(name="Version", icon_url="https://cdn.discordapp.com/attachments/499771950764261396/500485578794729482/discord_logo1600.png")
 	await bot.ssay(embed=embed)
 	
 ##############################################################################################################################
@@ -162,7 +162,7 @@ async def server(ctx):
 	 else:
 	  members += 1
 	  total += 1
-	embed = discord.Embed(title="📋 Server Info", description="Here's what I could find.", color=0xffafc9)
+	embed = discord.Embed(title="Server Info", description="Here's what I could find.", color=0xffafc9)
 	embed.set_thumbnail(url=ctx.message.server.icon_url)
 	embed.add_field(name="Name:", value=ctx.message.server.name, inline=True)
 	embed.add_field(name="ID:", value=ctx.message.server.id, inline=True)
@@ -179,7 +179,7 @@ async def server(ctx):
 
 @bot.command(pass_context=True)
 async def user(ctx, user: discord.Member):
-	embed = discord.Embed(title="📋 User Info", description="Here's what I could find.", color=0xffafc9)
+	embed = discord.Embed(title="User Info", description="Here's what I could find.", color=0xffafc9)
 	embed.set_thumbnail(url=user.avatar_url)
 	embed.add_field(name="Name", value=user, inline=True)
 	embed.add_field(name="ID", value=user.id, inline=True)
@@ -191,7 +191,7 @@ async def user(ctx, user: discord.Member):
 	
 @bot.command(pass_context=True)
 async def avatar(ctx, user: discord.Member):
-	embed = discord.Embed(title="🖼️ User Avatar", description=f"Here it is {user.name}'s profile pic",color=0xffafc9)
+	embed = discord.Embed(title="User Avatar", description=f"Here it is {user.name}'s profile pic",color=0xffafc9)
 	embed.set_image(url=user.avatar_url)
 	await bot.say(embed=embed)
 	
@@ -227,17 +227,17 @@ async def about(ctx):
 	
 @bot.command(pass_context=True)
 async def invite(ctx):
-	embed = discord.Embed(description="📨 If you'd like to add MikiBot to your server, go to our website here: https://relykxdiscord.wixsite.com/mikibot", color=0xffafc9)
+	embed = discord.Embed(description="If you'd like to add MikiBot to your server, go to our website here: https://relykxdiscord.wixsite.com/mikibot", color=0xffafc9)
 	await bot.say(embed=embed)
 	
 @bot.command(pass_context=True)
 async def vote(ctx):
-	embed = discord.Embed(description="📥 You can vote here: \nhttps://discordbots.org/bot/496214977267630080/vote", color=0xffafc9)
+	embed = discord.Embed(description="You can vote here: \nhttps://discordbots.org/bot/496214977267630080/vote", color=0xffafc9)
 	await bot.say(embed=embed)
 	
 @bot.command(pass_context=True)
 async def donate(ctx):
-	embed = discord.Embed(title="💵 Patreon", description="You can donate here: \nhttps://www.patreon.com/join/mikidiscord?", color=0xffafc9)
+	embed = discord.Embed(title="Patreon", description="You can donate here: \nhttps://www.patreon.com/join/mikidiscord?", color=0xffafc9)
 	embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/499771950764261396/513936104357888000/icon_color_variations.jpg")
 	await bot.say(embed=embed)
 	
@@ -284,7 +284,7 @@ async def greet(ctx):
 
 @bot.command(pass_context=True)
 async def kawaii(ctx):
-	embed = discord.Embed(title="💠 Kawaii Emoji", description="Find more here: https://kawaiiface.net/", color=0xffafc9,)
+	embed = discord.Embed(title="Kawaii Emoji", description="Find more here: https://kawaiiface.net/", color=0xffafc9,)
 	embed.add_field(name="Happy", value="`(✿◠‿◠)` `≧◡≦` `(▰˘◡˘▰)` `(●´ω｀●)`\n`(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧` `（ミ￣ー￣ミ）` `(づ｡◕‿‿◕｡)づ`\n`✌.ʕʘ‿ʘʔ.✌` `◎[▪‿▪]◎`", inline=False)
 	embed.add_field(name="Sad", value="`ಥ_ಥ` `┐(‘～'；)┌` `◄.►` `(◕︵◕)`\n`v( ‘.’ )v` `ਉ_ਉ` `o(╥﹏╥)o` `●︿●` `(∩︵∩)`", inline=False)
 	embed.add_field(name="Mad", value="`〴⋋_⋌〵` `(◣_◢)` `☉▵☉凸` `ↁ_ↁ`\n`╚(•⌂•)╝` `ᇂﮌᇂ)` `ლ(́◉◞౪◟◉‵ლ`\n`(┛◉Д◉)┛彡┻━┻ `", inline=False)
@@ -308,14 +308,14 @@ async def gif(ctx):
 @bot.command(pass_context=True)
 async def diceroll(ctx):
 	randomlist = ['1','2','3','4','5','6',]
-	embed = discord.Embed(title ="🎲 Dice Roll", description="*rolls a dice*", color=0xffafc9,)
+	embed = discord.Embed(title ="Dice Roll", description="*rolls a dice*", color=0xffafc9,)
 	embed.add_field(name="You rolled a dice and it landed on...", value="Side: **%s**" %(random.choice(randomlist),))
 	await bot.say(embed=embed)
 	
 @bot.command(pass_context=True)
 async def coinflip(ctx):
 	randomlist = ['Heads','Tails',]
-	embed = discord.Embed(title ="💰 Coin Flip", description="*flips a coin*", color=0xffafc9,)
+	embed = discord.Embed(title ="Coin Flip", description="*flips a coin*", color=0xffafc9,)
 	embed.add_field(name="You flipped a coin and it landed on", value="Face: **%s**" %(random.choice(randomlist),))
 	await bot.say(embed=embed)
 	
@@ -342,7 +342,7 @@ async def eightball(ctx):
                       'Outlook not so good.',
                       'Very doubtful.',
                      ]
-	embed = discord.Embed(title ="🎱 8 Ball", description="*shakes the 8 Ball up...*", color=0xffafc9,)
+	embed = discord.Embed(title ="8 Ball", description="*shakes the 8 Ball up...*", color=0xffafc9,)
 	embed.add_field(name="You shook the 8 ball and it shows you...", value="Answer: **%s**" %(random.choice(randomlist),))
 	await bot.say(embed=embed)
 	
@@ -351,29 +351,38 @@ async def eightball(ctx):
 ##############################################################################################################################
 	
 @bot.command(pass_context=True)
-async def commands(ctx):
-	embed = discord.Embed(title="📖 General", color=0xffafc9)
-	embed.add_field(name="serverinfo", value="Displays the info of the current server.", inline=False)
-	embed.add_field(name="userinfo", value="Displays a profile of the mentioned user.", inline=False)
-	embed.add_field(name="avatar", value="Dusplays the profile pic of the mentioned user.", inline=False)
-	embed.add_field(name="kick", value="Kicks the mentioned user.", inline=False)
-	embed.add_field(name="clear", value="Clears a specific amount of messages in a channel.", inline=False)
-	embed.add_field(name="about", value="Shows the About description of MikiBot.", inline=False)
-	embed.add_field(name="servercount", value="Shows how many servers this bot occupies.", inline=False)
-	embed.add_field(name="invite", value="Sends the invite to add MikiBot to your server.", inline=False)
-	embed.add_field(name="vote", value="Vote for MikiBot.", inline=False)
-	embed.add_field(name="donate", value="Donate to MikiBot.", inline=False)
-	embed.add_field(name="time", value="Displays the current time of the server.", inline=False)
+async def help(ctx):
+	embed = discord.Embed(description="All commands under the Admin categorie:", color=0xffafc9)
+	embed.set_author(name="Admin", icon_url="https://cdn.discordapp.com/attachments/499771950764261396/500485578794729482/discord_logo1600.png")
+	embed.add_field(name="ban", value="Bans the mentioned user",inline=True)
+	embed.add_field(name="unban", value="Unbans the mentioned user",inline=True)
+	embed.add_field(name="kick", value="Kicks the mentioned user",inline=True)
+	embed.add_field(name="clear", value="Clears a specific amount of messages in a channel",inline=True)
+	embed.add_field(name="ping", value="Displays current ping of MikiBot",inline=True)
+	embed.add_field(name="count", value="Displays the amount of members in the current server",inline=True)
 	await bot.say(embed=embed)
-	embed = discord.Embed(title="😜 Fun", color=0xffafc9)
-	embed.add_field(name="hi", value='Generates a response, for example: "hai ^-^"', inline=False)
-	embed.add_field(name="kawaii", value="Displays multiple different kawaii emoji", inline=False)
-	embed.add_field(name="wallpaper", value="Generates a random wallpaper.", inline=False)
-	embed.add_field(name="gif", value="Generates a random gif.", inline=False)
-	embed.add_field(name="ping", value="Pongs the user's ping.", inline=False)
-	embed.add_field(name="dicroll", value="Rolls a six sided die.", inline=False)
-	embed.add_field(name="coinflip", value="Flips a coin, could be heads could be tails.", inline=False)
-	embed.add_field(name="eightball", value="Ask a question and shake the 8 Ball.", inline=False)
+	embed = discord.Embed(description="All commands under the General categorie:", color=0xffafc9)
+	embed.set_author(name="General", icon_url="https://cdn.discordapp.com/attachments/499771950764261396/500485578794729482/discord_logo1600.png")
+	embed.add_field(name="server", value="Displays the info of the current server", inline=True)
+	embed.add_field(name="user", value="Displays a profile of the mentioned user", inline=True)
+	embed.add_field(name="avatar", value="Displays the profile pic of the mentioned user", inline=True)
+	embed.add_field(name="about", value="Displays the About description of MikiBot",inline=True)
+	embed.add_field(name="invite", value="Sends the invite link to add MikiBot to your server", inline=True)
+	embed.add_field(name="vote", value="Sends the voting link for MikiBot", inlin=True)
+	embed.add_field(name="donate", value="Sends the donate link for Mikibot", inline=True)
+	await bot.say(embed=embed)
+	embed = discord.Embed(description="All commands under the Fun categorie:", color=0xffafc9)
+	embed.set_author(name="Fun", icon_url="https://cdn.discordapp.com/attachments/499771950764261396/500485578794729482/discord_logo1600.png")
+	embed.add_field(name="google", value="-", inline=True)
+	embed.add_field(name="youtube", value="-", inline=True)
+	embed.add_field(name="greet", value="Generates a response, for example: 'hai ^-^'", inline=True)
+	embed.add_field(name="time", value="Displays the current time of the server", inline=True)
+	embed.add_field(name="kawaii", value="Displays multiple different kawaii emoji", inline=True)
+	embed.add_field(name="wallpaper", value="Generates a random wallpaper", inline=True)
+	embed.add_field(name="gif", value="Generates a random gif", inline=True)
+	embed.add_field(name="diceroll", value="Rolls a six sided die", inline=True)
+	embed.add_field(name="coinflip", value="Flips a coin, could be heads could be tails", inline=True)
+	embed.add_field(name="eightball", value="Ask a question and shake the 8 Ball", inline=True)
 	await bot.say(embed=embed)
 	
 ##############################################################################################################################
@@ -395,39 +404,5 @@ async def serverlist(ctx):
 	for x in range(len(serv)):
 	 embed = discord.Embed(title=serv[x-1].name, description=f"● ServerID: {serv[x-1].id}", color=0x7289da)
 	 await bot.say(embed=embed)
-
-@bot.command(pass_context=True)
-async def test(ctx):
-	embed = discord.Embed(description="All commands under the Admin categorie:", color=0xffafc9)
-	embed.set_author(name="Admin", icon_url="https://cdn.discordapp.com/attachments/499771950764261396/500485578794729482/discord_logo1600.png")
-	embed.add_field(name="ban", value="-",inline=True)
-	embed.add_field(name="unban", value="-",inline=True)
-	embed.add_field(name="kick", value="-",inline=True)
-	embed.add_field(name="clear", value="-",inline=True)
-	embed.add_field(name="ping", value="-",inline=True)
-	embed.add_field(name="count", value="-",inline=True)
-	await bot.say(embed=embed)
-	embed = discord.Embed(description="All commands under the General categorie:", color=0xffafc9)
-	embed.set_author(name="General", icon_url="https://cdn.discordapp.com/attachments/499771950764261396/500485578794729482/discord_logo1600.png")
-	embed.add_field(name="server", value="-",inline=True)
-	embed.add_field(name="user", value="-",inline=True)
-	embed.add_field(name="avatar", value="-",inline=True)
-	embed.add_field(name="about", value="-",inline=True)
-	embed.add_field(name="invite", value="-",inline=True)
-	embed.add_field(name="donate", value="-",inline=True)
-	await bot.say(embed=embed)
-	embed = discord.Embed(description="All commands under the Fun categorie:", color=0xffafc9)
-	embed.set_author(name="Funr", icon_url="https://cdn.discordapp.com/attachments/499771950764261396/500485578794729482/discord_logo1600.png")
-	embed.add_field(name="google", value="-",inline=True)
-	embed.add_field(name="youtube", value="-",inline=True)
-	embed.add_field(name="greet", value="-",inline=True)
-	embed.add_field(name="time", value="-",inline=True)
-	embed.add_field(name="kawaii", value="-",inline=True)
-	embed.add_field(name="wallpaper", value="-",inline=True)
-	embed.add_field(name="gif", value="-",inline=True)
-	embed.add_field(name="diceroll", value="-",inline=True)
-	embed.add_field(name="coinflip", value="-",inline=True)
-	embed.add_field(name="eightball", value="-",inline=True)
-	await bot.say(embed=embed)
 	
 bot.run(os.getenv("BOT_TOKEN"))
