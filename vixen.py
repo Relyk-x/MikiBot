db7b030e7ce665cfe9e460889e63f13002d18bc0
Relyk_x <43947707+Relyk-x@users.noreply.github.com>
2019-01-25 17:10:50 +0800

    Update vixen.py

--- vixen.py
+++ vixen.py
@@ -602,14 +602,12 @@ async def serverlist(ctx):
 
 @bot.command(pass_context=True)
 async def welcome(ctx):
-	embed = discord.Embed(colo=0xffffff,)
-	embed.set_image(url="https://cdn.discordapp.com/attachments/538269945096568841/538274222078951455/welcome.png")
-	embed.set_image(url="http://3.bp.blogspot.com/-oDR9Iz4RnaI/ViP_OR0M4XI/AAAAAAAAfJo/5rayU0QAOSo/s1600/music-equalizer-png-365-funny-pics.jpg")
-	embed.add_field(name="Relyk_x#2896", value="Welcome to my server!\nInvite your friends!", inline=True)
-	embed.add_field(name="Server", value='<:blurple:535747202740518922><:discord:535748146761039872> Invite: https://discord.gg/bzyVpbd\n\n\n<:white:535747728614096906>:zap: Firstly please check out the #rules & #tos for all the information needed.\n\n<:white:535747728614096906>:zap: Also check out the #self-roles channel to register.\n\n<:white:535747728614096906>:zap: Lookout for notifications in #announcements  for new info.\n\n<:white:535747728614096906>:zap: If you have any problems, suggestions, or need any help feel free to ask the staff in #support.\n\n<:white:535747728614096906>:zap: Lastly why not say "Hello" in the #general-chat.', inline=True)
-	await bot.say(embed=embed)
+	await bot.say(url="https://cdn.discordapp.com/attachments/538269945096568841/538274222078951455/welcome.png")
+	await bot.say(url="http://3.bp.blogspot.com/-oDR9Iz4RnaI/ViP_OR0M4XI/AAAAAAAAfJo/5rayU0QAOSo/s1600/music-equalizer-png-365-funny-pics.jpg")
+	await bot.say("**Welcome to my server!**\n**Invite your friends!")
+	await bot.say('<:blurple:535747202740518922><:discord:535748146761039872> Invite: https://discord.gg/bzyVpbd\n\n\n<:white:535747728614096906>:zap: Firstly please check out the #rules & #tos for all the information needed.\n\n<:white:535747728614096906>:zap: Also check out the #self-roles channel to register.\n\n<:white:535747728614096906>:zap: Lookout for notifications in #announcements  for new info.\n\n<:white:535747728614096906>:zap: If you have any problems, suggestions, or need any help feel free to ask the staff in #support.\n\n<:white:535747728614096906>:zap: Lastly why not say "Hello" in the #general-chat.')
 
-##############################################################################################################################
+	##############################################################################################################################
 # ℹ️ | H E L P - C O M M A N D S	
 ##############################################################################################################################
 
