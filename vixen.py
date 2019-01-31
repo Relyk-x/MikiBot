94fa560a651eae1de86bf8371519a6358ac5a383
Relyk_x <43947707+Relyk-x@users.noreply.github.com>
2019-01-31 15:16:59 +0800

    Update vixen.py

--- vixen.py
+++ vixen.py
@@ -604,7 +604,7 @@ async def serverlist(ctx):
 @commands.has_permissions(administrator = True)
 async def welcome(ctx):
 	embed = discord.Embed(color=0x7289da,)
-	embed.set_image(url="https://cdn.discordapp.com/attachments/538265900546260993/540429273551339540/welcomes.png")
+	embed.set_image(url="https://cdn.discordapp.com/attachments/535747082192027651/540430139578777600/welcomes.png")
 	# https://cdn.discordapp.com/attachments/535747082192027651/539772749091045376/welc.png
 	await bot.say(embed=embed)
 	
